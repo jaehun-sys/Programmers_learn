13b320f14019a995181cac3e70d86222b5431311
jaehun-sys <us03098@gmail.com>
2021-10-21 23:54:57 +0900

    2021-10-21-v1 1. change 했을 경우 결과 반영

---
+++ src/오픈채팅방.java
@@ -0,0 +1,44 @@
+
+public class 오픈채팅방 {
+	
+	public String[] solution(String[] record) {
+        String[] answer = {};
+
+        String[][] recordArr = new String[record.length][];
+        for(int i=0; i<record.length; i++) {
+        	recordArr[i] = record[i].split(" ");
+
+        	//첫 단어 확인
+        	if(recordArr[i][0].equals("Enter")) {
+        		
+        	}
+        	else if(recordArr[i][0].equals("Leave")) {
+        		
+        	}
+        	else {	//change
+        		for(int j=0; j<record.length; j++) {
+        			if(recordArr[i][1].equals(recordArr[j][1])) {
+        				recordArr[j][2] = recordArr[i][2];	//이름 바꾸기
+        			}
+        		}
+        	}
+
+        } 
+        for(int i=0; i<recordArr.length; i++) {
+        	for(int j=0; j<recordArr[i].length; j++) {
+        		System.out.print(recordArr[i][j] + " ");
+        	}
+        	System.out.println();
+        }
+        return answer;
+    }
+	
+	public static void main(String[] args) {
+		String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo",
+				"Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
+		
+		오픈채팅방 s = new 오픈채팅방();
+		s.solution(record);
+
+	}
+}
