31f2c86ec68e532f69b82a766b1d1b1f2c350d34
jaehun-sys <us03098@gmail.com>
2021-11-19 23:24:19 +0900

    2021-11-24-v1 타겟넘버 - 스택,큐 연습

---
+++ src/타겟넘버.java
@@ -0,0 +1,23 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Å¸°Ù³Ñ¹ö {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue<Integer> queue = new LinkedList<>();
+
+		
+		Stack<Integer> stack = new Stack<>();
+		stack.push(4);
+		stack.push(3);
+		stack.push(2);
+		stack.push(5);
+		System.out.println(stack);
+		stack.pop();
+		System.out.println(stack);
+
+	}
+
+}
