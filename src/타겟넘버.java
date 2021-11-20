41643467b6fda8c8d93b10894fc35653065a00ea
jaehun-sys <us03098@gmail.com>
2021-11-20 23:57:43 +0900

    2021-11-20-v1 타겟 넘버 완료(dfs)

--- src/타겟넘버.java
+++ src/타겟넘버.java
@@ -4,6 +4,28 @@ import java.util.Stack;
 
 public class Å¸°Ù³Ñ¹ö {
 
+	public int solution(int[] numbers, int target) {
+        int answer = 0;
+
+        answer = dfs(numbers, target, 0,0);
+        
+        return answer;
+    }
+    
+    public int dfs(int[] numbers, int target, int sum, int idx){
+        if(idx == numbers.length){
+            if(sum == target){
+                return 1;
+            }
+            else
+                return 0;
+        }
+        else{
+            return dfs(numbers, target, sum+numbers[idx], idx+1)
+                + dfs(numbers, target, sum-numbers[idx], idx+1);
+        }
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Queue<Integer> queue = new LinkedList<>();
