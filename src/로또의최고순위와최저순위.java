cc641cd90e6b3ab3af19e7ba3ded5cdbd5353dee
jaehun-sys <us03098@gmail.com>
2021-11-23 23:53:06 +0900

    2021-11-23-v1 : 로또의 최고 순위와 최저 순위

--- src/로또의최고순위와최저순위.java
+++ src/로또의최고순위와최저순위.java
@@ -1,6 +1,14 @@
 
 public class 로또의최고순위와최저순위 {
 
+	public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {};
+        
+        
+        
+        return answer;
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
