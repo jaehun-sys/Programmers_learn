ad364c85d11063f675cd018ec067a4f991c8dbaf
jaehun-sys <us03098@gmail.com>
2021-11-25 09:58:43 +0900

    2021-11-25-v1 없는 숫자 더하기 완료

---
+++ src/없는숫자더하기.java
@@ -0,0 +1,36 @@
+
+public class 없는숫자더하기 {
+	
+
+    public int solution(int[] numbers) {
+        int answer = -1;
+        
+        int[] numeral = {0,1,2,3,4,5,6,7,8,9};
+        
+        for(int i=0; i<numbers.length; i++){    //하나하나 비교.. 시간복잡도는??
+            for(int j=0; j<numeral.length; j++){
+                if(numbers[i]==numeral[j]){
+                    numeral[j] = 0;
+                }
+            }
+        }
+        
+        int sum = 0;
+        for(int i=0; i<numeral.length; i++){
+            sum += numeral[i];
+        }
+        answer = sum;
+        
+        return answer;
+    }
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		없는숫자더하기 s = new 없는숫자더하기();
+		int[] numbers = {1,2,3,4,6,7,8,0};	//답 : 14
+		System.out.println(s.solution(numbers));
+
+	}
+
+}
