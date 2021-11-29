573cd489595929e2c295927ec70866b74e660a20
jaehun-sys <us03098@gmail.com>
2021-11-29 22:25:05 +0900

    2021-11-29-v1 기능개발

--- src/기능개발.java
+++ src/기능개발.java
@@ -1,8 +1,10 @@
+import java.util.Stack;
+
 public class 기능개발 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Stack<Integer> stack = new Stack<>();
 
 	}
 
