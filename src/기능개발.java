14399d165c3e488622d61f5d03170fb8a4fe25bc
KOSTA <KOSTA@DESKTOP-SN6M77H>
2021-12-02 15:48:42 +0900

    2021-12-02-v1 기능개발 완료

--- src/기능개발.java
+++ src/기능개발.java
@@ -1,11 +1,35 @@
 import java.util.Stack;
 
-public class ±â´É°³¹ß {
+public class ê¸°ëŠ¥ê°œë°œ {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Stack<Integer> stack = new Stack<>();
-		
+		for (int i = progresses.length - 1; i >= 0; i--)
+			stack.add((100 - progresses[i]) / speeds[i] + ((100 - progresses[i]) % speeds[i] > 0 ? 1 : 0));
+
+		List<Integer> s = new ArrayList<Integer>();
+
+		while (!stack.isEmpty()) {
+			int cnt = 1;
+
+			int top = stack.pop();
+
+			while (!stack.isEmpty() && stack.peek() <= top) {
+				cnt++;
+				stack.pop();
+			}
+
+			s.add(cnt);
+		}
+
+		int[] answer = new int[s.size()];
+
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = s.get(i);
+		}
+
+		return answer;
 
 	}
 
