9ec415799e709351c2e7312c7cf2a5de7d4fe273
jaehun-sys <us03098@gmail.com>
2021-12-03 20:31:18 +0900

    2021-12-03-v1 기능개발 연구

--- src/기능개발.java
+++ src/기능개발.java
@@ -1,14 +1,14 @@
 import java.util.Stack;
+import java.util.ArrayList;
 
 public class 기능개발 {
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public int[] solution(int[] progresses, int[] speeds) {
 		Stack<Integer> stack = new Stack<>();
 		for (int i = progresses.length - 1; i >= 0; i--)
 			stack.add((100 - progresses[i]) / speeds[i] + ((100 - progresses[i]) % speeds[i] > 0 ? 1 : 0));
 
-		List<Integer> s = new ArrayList<Integer>();
+		ArrayList<Integer> s = new ArrayList<Integer>();
 
 		while (!stack.isEmpty()) {
 			int cnt = 1;
@@ -30,6 +30,11 @@ public class 기능개발 {
 		}
 
 		return answer;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
 
 	}
 
--- src/로또의최고순위와최저순위.java
+++ src/로또의최고순위와최저순위.java
@@ -4,8 +4,8 @@ public class 로또의최고순위와최저순위 {
 
 	public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
-        int count = 0;  //일치하는 번호 개수
-        int zeroCount = 0;  //0 개수
+        int count = 0;  //�씪移섑븯�뒗 踰덊샇 媛쒖닔
+        int zeroCount = 0;  //0 媛쒖닔
         
         for(int i=0; i<lottos.length; i++){
             for(int j=0; j<win_nums.length; j++){
@@ -18,11 +18,11 @@ public class 로또의최고순위와최저순위 {
             }
         }
         
-        int rank = 7-count; //최저 순위
+        int rank = 7-count; //理쒖� �닚�쐞
         if(rank>6){
             rank = 6;
         }
-        int maxRank = 7-count-zeroCount;    //최고 순위
+        int maxRank = 7-count-zeroCount;    //理쒓퀬 �닚�쐞
         if(maxRank>6){
             maxRank=6;
         }
@@ -36,7 +36,7 @@ public class 로또의최고순위와최저순위 {
 		// TODO Auto-generated method stub
 		int[] lottos = {44,1,0,0,31,25};
 		int[] win_nums = {31,10,45,1,6,19};
-		//답:3,5
+		//�떟:3,5
 		
 		로또의최고순위와최저순위 s = new 로또의최고순위와최저순위();
 		System.out.println(Arrays.toString(s.solution(lottos, win_nums)));
