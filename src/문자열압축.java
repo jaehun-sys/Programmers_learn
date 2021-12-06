74f104a3c100acce32912cd604758bffc01bc91b
KOSTA <KOSTA@DESKTOP-SN6M77H>
2021-12-06 17:20:37 +0900

    2021-12-06-v1 문자열 압축

--- src/문자열압축.java
+++ src/문자열압축.java
@@ -4,7 +4,9 @@ public class 문자열압축 {
 	public int solution(String s) {
         int answer = 0;
         
-        
+        for(int i=0; i<s.length(); i++) {
+        	
+        }
         
         return answer;
     }
