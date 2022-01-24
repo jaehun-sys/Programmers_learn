6f121112e43685e7acef50cf246da2b463da358d
jaehun-sys <us03098@gmail.com>
2022-01-24 23:13:34 +0900

    2022-01-24-v1 괄호변환 시작

---
+++ src/괄호변환.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class 괄호변환 {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		기능개발 s = new 기능개발();
+		int[] a = {93,30,55};
+		int[] b = {1,30,5};
+		System.out.println(Arrays.toString(s.solution(a, b)));
+
+	}
+}
