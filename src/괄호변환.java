9e045e461ee453d8eccd9e23682166e0ac1360aa
jaehun-sys <us03098@gmail.com>
2022-01-27 23:26:43 +0900

    2022-01-27-v1 괄호변환

--- src/괄호변환.java
+++ src/괄호변환.java
@@ -20,10 +20,13 @@ public class 괄호변환 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		기능개발 s = new 기능개발();
-		int[] a = {93,30,55};
-		int[] b = {1,30,5};
-		System.out.println(Arrays.toString(s.solution(a, b)));
+		괄호변환 s = new 괄호변환();
+//		int[] a = {93,30,55};
+//		int[] b = {1,30,5};
+		String a = "(()())()";
+		String b = ")(";
+		String c = "()))((()";
+		System.out.println(s.solution(a));
 
 	}
 }
