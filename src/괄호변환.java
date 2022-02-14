1964af12527e7c2ad146a232a019b03645e500bd
jaehun-sys <us03098@gmail.com>
2022-02-14 23:39:07 +0900

    2022-02-14-v1

--- src/괄호변환.java
+++ src/괄호변환.java
@@ -5,7 +5,7 @@ public class 괄호변환 {
 
     public String solution(String p) {
         String answer = ""; 
-        int count = 0; 
+        int count = 0;
         
         for(int i=0; i<p.length(); i++) {
         	if(p.charAt(i)=='(') {
