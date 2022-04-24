90d667a41e1dfd265039c5f17bfe2cd58d000932
jaehun-sys <us03098@gmail.com>
2022-04-24 23:58:53 +0900

    2022-04-24-v1

--- src/괄호변환.java
+++ src/괄호변환.java
@@ -8,7 +8,7 @@ public class 괄호변환 {
         return answer;
     }
 	
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 		괄호변환 s = new 괄호변환();
 //		int[] a = {93,30,55};
 //		int[] b = {1,30,5};
